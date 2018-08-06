460c499c83e76add0ead043e963749a91491446f
subJ <sucuilin27@sina.com>
2018-08-06 22:39:43 +0800

    init

--- src/main/java/com/insu/house/config/DruidConfig.java
+++ src/main/java/com/insu/house/config/DruidConfig.java
@@ -1,44 +1,42 @@
 package com.insu.house.config;
 
-import com.alibaba.druid.filter.Filter;
-import com.alibaba.druid.filter.stat.StatFilter;
-import com.alibaba.druid.pool.DruidDataSource;
-import com.alibaba.druid.support.http.StatViewServlet;
-import com.google.common.collect.Lists;
+import java.sql.SQLException;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-/**
- * 配置Druid连接池
- *
- */
+import com.alibaba.druid.filter.Filter;
+import com.alibaba.druid.filter.stat.StatFilter;
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.google.common.collect.Lists;
+
 @Configuration
 public class DruidConfig {
 
-    @ConfigurationProperties(prefix = "spring.druid")
-    @Bean(initMethod = "init",destroyMethod = "close")
-    public DruidDataSource dataSource(){
-        DruidDataSource dataSource1 = new DruidDataSource();
-        dataSource1.setProxyFilters(Lists.newArrayList(statFilter()));
-        return dataSource1;
-    }
-
-    @Bean
-    public Filter statFilter(){
-        StatFilter filter = new StatFilter();
-        filter.setSlowSqlMillis(2000);  // 设置慢时间
-        filter.setLogSlowSql(true);     // 显示慢日志
-        filter.setMergeSql(true);       // 将日志合并起来
-        return filter;
-    }
-
-   // 监控
-    @Bean
-    public ServletRegistrationBean servletRegistrationBean() {
-        return new ServletRegistrationBean(new StatViewServlet(),"/druid/*");
-    }
-
-
+	@ConfigurationProperties(prefix="spring.druid")
+	@Bean(initMethod="init",destroyMethod="close")
+	public DruidDataSource dataSource(Filter statFilter) throws SQLException{
+		DruidDataSource dataSource = new DruidDataSource();
+		dataSource.setProxyFilters(Lists.newArrayList(statFilter()));
+		return dataSource;
+	}
+	
+	@Bean
+	public Filter statFilter(){
+		StatFilter filter = new StatFilter();
+		filter.setSlowSqlMillis(5000);
+		filter.setLogSlowSql(true);
+		filter.setMergeSql(true);
+		return filter;
+	}
+	
+	
+	@Bean
+	public ServletRegistrationBean servletRegistrationBean(){
+		return new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+	}
+	
 }
